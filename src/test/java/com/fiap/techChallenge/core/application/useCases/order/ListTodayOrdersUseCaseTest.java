c8bef6e6e94bf20dac5c8cfbe651d397abc912ea
leynerbueno <leyner_henrique@hotmail.com.br>
2025-09-02 16:47:02 -0300

    Update ListTodayOrdersUseCaseTest.java

--- src/test/java/com/fiap/techChallenge/core/application/useCases/order/ListTodayOrdersUseCaseTest.java
+++ src/test/java/com/fiap/techChallenge/core/application/useCases/order/ListTodayOrdersUseCaseTest.java
@@ -1,24 +1,29 @@
 package com.fiap.techChallenge.core.application.useCases.order;
 
-import com.fiap.techChallenge.core.application.dto.order.OrderWithStatusAndWaitMinutesDTO;
-import com.fiap.techChallenge.core.domain.enums.OrderStatus;
-import com.fiap.techChallenge.core.gateways.order.OrderGateway;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
-
-import java.util.Collections;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import com.fiap.techChallenge.core.application.dto.order.OrderWithStatusAndWaitMinutesDTO;
+import com.fiap.techChallenge.core.domain.enums.OrderStatus;
+import com.fiap.techChallenge.core.gateways.order.OrderGateway;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Testes para ListTodayOrdersUseCase")
